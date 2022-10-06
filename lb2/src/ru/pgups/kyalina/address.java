aaa3bd063dc2cf4d04450a1a58e96bfe71dbba65
Kyalina <nastiyk128@gmail.com>
2022-10-06 21:45:46 +0300

    lb2

---
+++ lb2/src/ru/pgups/kyalina/address.java
@@ -0,0 +1,46 @@
+package ru.pgups.kyalina;
+
+public class address {
+
+    private String city;
+    private String street;
+    private int house_number;
+
+    public void Address(String city, String street, int house_number){
+        this.city = city;
+        this.street = street;
+        this.house_number = house_number;
+    }
+
+    public void Address(){
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse_number() {
+        return house_number;
+    }
+
+    public void setHouse_number(int house_number) {
+        if (house_number > 0) {
+            this.house_number = house_number;
+        }else{
+            System.out.println("House number cannot be less than 0");
+        }
+    }
+}
+
