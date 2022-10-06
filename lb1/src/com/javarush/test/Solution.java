aa427b52b91fa0b56996c8a23d541c5bcab071a3
ViktoriaLepeshkina <viktorialep01@mail.ru>
2022-10-06 23:43:31 +0300

    lb1

--- lb1/src/com/javarush/test/Solution.java
+++ lb1/src/com/javarush/test/Solution.java
@@ -2,7 +2,7 @@ package com.javarush.test;
 
 public class Solution {
     public static void main(String[] ards) {
-        System.out.println("Hello Viktoria !");
+        System.out.println("Hello Viktoria!");
     }
 }
 
