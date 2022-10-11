e137acc4ee51fcf982e22f3acbfbd8e28ed45efe
HirossaNCBS2 <yndx-karpenkova-a@yandex.ru>
2022-10-11 14:31:35 +0300

    Karpenkova_Labs1_2

--- Karpenkova_Labs/src/lab2/MainHotel.java
+++ Karpenkova_Labs/src/lab2/MainHotel.java
@@ -3,34 +3,34 @@ package lab2;
 public class MainHotel {
     public static void main(String[] args) {
 
-        Client Cheloveck = new Client("Ленидский","Анатолий" , "Борисович" ,  35 ,  "Муж");
-        System.out.println("Фамилия : " + Cheloveck.getSurname());
-        System.out.println("Имя : " + Cheloveck.getName());
-        System.out.println("Отчество : " + Cheloveck.getPatronymic());
-        System.out.println("Возраст : " + Cheloveck.getAge());
-        System.out.println("Пол : " + Cheloveck.getGender());
+        Client сheloveck = new Client("Ленидский","Анатолий" , "Борисович" ,  35 ,  "Муж");
+        System.out.println("Фамилия : " + сheloveck.getSurname());
+        System.out.println("Имя : " + сheloveck.getName());
+        System.out.println("Отчество : " + сheloveck.getPatronymic());
+        System.out.println("Возраст : " + сheloveck.getAge());
+        System.out.println("Пол : " + сheloveck.getGender());
 
-        Сommunication Svyaz = new Сommunication( "tolyanLenidskiy@mail.ru", 5438474);
-        System.out.println("Email : " + Svyaz.getEmail());
-        System.out.println("Номер телефона : " + Svyaz.getPhoneNumber());
+        Сommunication svyaz = new Сommunication( "tolyanLenidskiy@mail.ru", 5438474);
+        System.out.println("Email : " + svyaz.getEmail());
+        System.out.println("Номер телефона : " + svyaz.getPhoneNumber());
 
-        HotelRoom Room = new HotelRoom( "одноместный");
-        System.out.println("Вместимость номера : " + Room.getRoomCapacity());
+        HotelRoom room = new HotelRoom( "одноместный");
+        System.out.println("Вместимость номера : " + room.getRoomCapacity());
 
-        Category Nomera = new Category( "стандартный");
-        System.out.println("Категория номера : " + Nomera.getRoomClass());
+        Category nomera = new Category( "стандартный");
+        System.out.println("Категория номера : " + nomera.getRoomClass());
 
 
-        Food Stoimost = new Food( "включена");
-        System.out.println("Еда включена в стоимость : " + Stoimost.getIncludedInThePrice());
+        Food stoimost = new Food( "включена");
+        System.out.println("Еда включена в стоимость : " + stoimost.getIncludedInThePrice());
 
-        AdditionalServices Usluga = new AdditionalServices( "поздний выезд без доплаты",  1500);
-        System.out.println("Привилегии : " + Usluga.getPrivileges());
-        System.out.println("Бонусов начислено + : " + Usluga.getBonusPoints());
+        AdditionalServices usluga = new AdditionalServices( "поздний выезд без доплаты",  1500);
+        System.out.println("Привилегии : " + usluga.getPrivileges());
+        System.out.println("Бонусов начислено + : " + usluga.getBonusPoints());
 
-        Payment Oplata = new Payment( "Наличными",  50200);
-        System.out.println("Тип оплаты : " + Oplata.getTypeOfPayment());
-        System.out.println("Цена : " + Oplata.getCost());
+        Payment oplata = new Payment( "Наличными",  50200);
+        System.out.println("Тип оплаты : " + oplata.getTypeOfPayment());
+        System.out.println("Цена : " + oplata.getCost());
 
 
     }
