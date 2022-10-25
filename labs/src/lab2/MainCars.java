57b634a98a6bb0cf1e046f1bfd537a1c195eb12d
BlackRaison1 <d-who@inbox.ru>
2022-10-25 11:10:35 +0300

    Переделал camelcase

--- labs/src/lab2/MainCars.java
+++ labs/src/lab2/MainCars.java
@@ -4,45 +4,46 @@ public class MainCars {
 
     public static void main(String[] args) {
         System.out.println("АВТОМОБИЛЬ НИССАН!!!");
-        Cars Nissan = new Cars("Almera", 10, 150000);
-        System.out.println("Модель машины: " + Nissan.getModel());
-        System.out.println("Возраст авто: " + Nissan.getAge());
-        System.out.println("Пробеш авто: " + Nissan.getProbeg());
 
-        Client First = new Client("Денчик", 89218845345L);
+        Cars nissan = new Cars("Almera", 10, 150000);
+        System.out.println("Модель машины: " + nissan.getModel());
+        System.out.println("Возраст авто: " + nissan.getAge());
+        System.out.println("Пробеш авто: " + nissan.getProbeg());
 
-        System.out.println("Имя клиента: " + First.getName());
-        System.out.println("Телефон клиента: " + First.getPhone());
+        Client first = new Client("Денчик", 89218845345L);
 
-        Repair One  = new Repair(1500, 25000, 5200);
+        System.out.println("Имя клиента: " + first.getName());
+        System.out.println("Телефон клиента: " + first.getPhone());
 
-        System.out.println("Цена замены масла: " + One.getMaslo());
-        System.out.println("Цена ремонта двигателя: " + One.getDvig());
-        System.out.println("Цена шпаклёвки кузова: " + One.getShpak());
+        Repair one  = new Repair(1500, 25000, 5200);
 
-        Salon Vnutr = new Salon("Алькантара", "Кожа", 5);
+        System.out.println("Цена замены масла: " + one.getMaslo());
+        System.out.println("Цена ремонта двигателя: " + one.getDvig());
+        System.out.println("Цена шпаклёвки кузова: " + one.getShpak());
 
-        System.out.println("Отделка салона: " + Vnutr.getOtdelka());
-        System.out.println("Отделка кресел: " + Vnutr.getKresla());
-        System.out.println("Колличество мест: " + Vnutr.getMesta());
+        Salon vnutr = new Salon("Алькантара", "Кожа", 5);
 
-        Workers Sostav = new Workers("Никита", "Дима", "Арчи");
-        System.out.println("Слесарь: " + Sostav.getSles());
-        System.out.println("Монтажник: " + Sostav.getMont());
-        System.out.println("Администратор: " + Sostav.getAdmin());
+        System.out.println("Отделка салона: " + vnutr.getOtdelka());
+        System.out.println("Отделка кресел: " + vnutr.getKresla());
+        System.out.println("Колличество мест: " + vnutr.getMesta());
+
+        Workers sostav = new Workers("Никита", "Дима", "Арчи");
+        System.out.println("Слесарь: " + sostav.getSles());
+        System.out.println("Монтажник: " + sostav.getMont());
+        System.out.println("Администратор: " + sostav.getAdmin());
 
 
-        Payment P = new Payment("Карта");
+        Payment p = new Payment("Карта");
 
-        System.out.println("Ваш способ оплаты: "+ P.getPayment());
-        P.setPayment("Наличные");
-        System.out.println("Ваш новый способ оплаты: "+ P.getPayment());
+        System.out.println("Ваш способ оплаты: "+ p.getPayment());
+        p.setPayment("Наличные");
+        System.out.println("Ваш новый способ оплаты: "+ p.getPayment());
 
-        Status S = new Status("Не оплачено");
+        Status s = new Status("Не оплачено");
 
-        System.out.println("Статус оплаты: "+ S.getStatus());
-        S.setStatus("Оплачено");
-        System.out.println("Ваш новый статус оплаты: "+ S.getStatus());
+        System.out.println("Статус оплаты: "+ s.getStatus());
+        s.setStatus("Оплачено");
+        System.out.println("Ваш новый статус оплаты: "+ s.getStatus());
 
 
     }
