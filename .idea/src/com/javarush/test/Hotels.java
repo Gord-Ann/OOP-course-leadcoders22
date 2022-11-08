7a9f8c0d851538a7109a2a612828b38b1420864b
liyakuraeva <113944836+liyakuraeva@users.noreply.github.com>
2022-11-08 21:58:41 +0300

    Update Hotels.java

--- .idea/src/com/javarush/test/Hotels.java
+++ .idea/src/com/javarush/test/Hotels.java
@@ -1,64 +1,38 @@
 package com.javarush.test;
-public class HotelsRating {
-    private int reviews; //отзывы
-    private int popularity;//популярность
-    private int price; //Цена
-    private int service; //Удобства
+public class Hotels {
+private String name;
+private Long phone;
 
-    public HotelsRating(int reviews, int popularity, int price, int service){
-        this.reviews = reviews;
-        this.popularity = popularity;
-        this.price = price;
-        this.service = service;
-
-    }
-    public HotelsRating(){
-    }
-    public int getReviews() {
-        return reviews;
-    }
-    public void setReviews(int reviews){
-        if (reviews >= 0 && reviews <= 5){
-            this.reviews = reviews;
-        }else{
-            System.out.println("Rating cannot be less than 0 or more than 5");
+public Hotels(String name, Long phone) {
+        this.name = name;
+        this.phone = phone;
         }
 
-    }
-    public int getPopularity() {
-        return popularity;
-    }
-    public void setPopularity(int Popularity){
-        if (popularity >= 0 && popularity <= 5){
-            this.popularity = popularity;
-        }else{
-            System.out.println("Rating cannot be less than 0 or more than 5");
+public Hotels() {
         }
 
-    }
-
-    public int getPrice() {
-        return price;
-    }
-    public void setPrice(int Price){
-        if (price >= 0 && price <= 5){
-            this.price = Price;
-        }else{
-            System.out.println("Rating cannot be less than 0 or more than 5");
+public String getName() {
+        return name;
         }
 
-    }
-
-    public int getService() {
-        return service;
-    }
-    public void setService(int Service){
-        if (Service >= 0 && Service <= 20){
-            this.service = service;
-        }else{
-            System.out.println("Rating cannot be less than 0 or more than 20");
+public void setName(String name) {
+        this.name = name;
         }
 
-    }
+public Long getPhone() {
+        return phone;
+        }
 
-}
+public void setPhone(Long phone) {
+        if (phone != 11) {
+        this.phone = phone;
+        } else {
+        System.out.println("Ошибка. Проверьте корректность ввода");
+        }
+        }
+        }
+
+
+
+
+
