7dd9e2c919cfedcce2b67c9fc5509b19ca3faeab
ViktoriaLepeshkina <viktorialep01@mail.ru>
2022-12-15 18:29:46 +0300

    lb

--- lb/src/ProblemMessage.java
+++ lb/src/ProblemMessage.java
@@ -1,10 +1,23 @@
 class ProblemMessage extends Request {
+    private String photo;
+    private String address;
+    private String comment;
 
-
-    public ProblemMessage (String topic, String photo, String address, String comment){
-        super (topic, photo, address, comment);
+    public ProblemMessage(String topic, String photo, String address, String comment) {
+        super(topic);
+        this.photo = photo;
+        this.address = address;
+        this.comment = comment;
     }
 
+    public void display()
+
+    {
+        System.out.println("Topic: " + getTopic());
+        System.out.println("Photo size: " + this.photo);
+        System.out.println("Mark on the map: " + this.address);
+        System.out.println("Photo comment: " + this.comment);
 
 
+    }
 }
--- lb/src/Request.java
+++ lb/src/Request.java
@@ -1,21 +1,18 @@
  class Request {
     private String topic;
-    private String photo;
-    private String address;
-    private String comment;
 
-    public Request(String topic, String photo, String address, String comment)
+
+    public Request(String topic)
     {
         this.topic = topic;
-        this.photo = photo;
-        this.address = address;
-        this.comment = comment;
+
     }
-    public void display() {
-        System.out.println("Topic: " + this.topic );
-        System.out.println("Photo size: " + this.photo);
-        System.out.println("Mark on the map: " + this.address);
-        System.out.println("Photo comment: " + this.comment);
+
+        public String getTopic() {
+            return topic;
+        }
+        public void setTopic (String topic){
+            this.topic = topic;
     }
 
 }
